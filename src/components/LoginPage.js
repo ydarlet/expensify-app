6d314c30da9cece0e34a71cd89179f2c79dde149
Yann Darlet <yann.darlet@gmail.com>
2018-07-08 21:25:50 +0200

    fix title login page and login button size

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,15 +5,15 @@ import { startLoginGoogle, startLoginFacebook } from '../actions/auth';
 export const LoginPage = ({ startLoginGoogle, startLoginFacebook }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Expensify App</h1>
+            <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button className="button button--facebook" onClick={startLoginFacebook}>
-                <i className="fab fa-facebook-f font-awesome"></i>
-                Login with Facebook
-            </button>
             <button className="button button--google" onClick={startLoginGoogle}>
                 <i className="fab fa-google font-awesome"></i>
                 Login with Google
+            </button>
+            <button className="button button--facebook" onClick={startLoginFacebook}>
+                <i className="fab fa-facebook-f font-awesome"></i>
+                Login with Facebook
             </button>
         </div>
     </div>
